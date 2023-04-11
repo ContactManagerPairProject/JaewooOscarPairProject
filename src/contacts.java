0c1ae6adb582eaa0ce8729e7c3615c4c64597079
Jaewoo Baek <jaewoo.baek01@gmail.com>
2023-04-11 14:37:47 -0500

    Switch driver

--- src/contacts.java
+++ src/contacts.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class contacts {
+    private String name;
+    private String phone;
+
+    public Contact(String name, String phone){
+        this.name = name;
+        this.phone = phone;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getPhoneNumber() {
+        return phone;
+    }
+    public void setPhone(String phone){
+        this.phone=phone;
+    }
+    public String toString() {
+        return name + "|" + phone;
+    }
 }
+
